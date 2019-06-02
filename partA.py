f76049520928b012c8931eb70507d163bfc8c9c9
Derick Vitancol <derickvitancol@gmail.com>
2019-06-02 23:41:48 +0800

    Part A Program

---
+++ partA.py
@@ -0,0 +1,8 @@
+class Account:
+
+    def setUsername(self,uname):
+        self.username = uname
+    def setPassword(self,pword):
+        self.password = pword
+    def setName(self,name):
+        self.name = name
