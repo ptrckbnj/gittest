fa84963e143f4266d78a474858f3043856276c17
ptrckbnj <patrickbanjao1@gmail.com>
2019-06-02 23:37:54 +0900

    Part B Program

---
+++ PartBprogram.py
@@ -0,0 +1,9 @@
+#userBasis = 'Patrick'
+#userPass = 'Banjao'
+
+userFile = open(r'userinfo.csv','r')
+
+user = Account()
+accountBasis = Account()
+uName = input("Username: ")
+pWord = input("Password: ")
