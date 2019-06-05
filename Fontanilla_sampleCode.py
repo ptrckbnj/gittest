ee8ccf64540fbb03058072fefe5bdcabf5dd2eaa
nktfontanilla <fontanilla_nichollekim13@yahoo.com>
2019-06-06 01:27:23 +0800

    Remoed files from Nicholle Kim branch

--- Fontanilla_sampleCode.py
+++
@@ -1,8 +0,0 @@
-print("Convert how long have you lived from years into days, minutes, and seconds.\n")
-name = input("Enter your name: ")
-age = int(input("Enter you age: ")
-days = age * 365
-minutes = age * 525948
-seconds = age * 31556926
-print("\n", name, "has been alive for ", days, " days ", minutes, " minutes and ", seconds " seconds.");
-
